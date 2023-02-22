689717dd2d939e95a1b1764129863e14561aac10
Andrei.Tyrin <andrei.tyrin@jetbrains.com>
2023-02-22 16:19:03 +0100

    latest published versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
 
     // https://kotlinlang.org/docs/releases.html#release-details
-    val kotlinVersion = "1.7.20"
+    val kotlinVersion = "1.8.20-Beta"
     // https://github.com/diffplug/spotless/tags
     val spotlessVersion = "6.11.0"
     // https://github.com/Kotlin/dokka/tags
-    val dokkaVersions = "1.5.30"
+    val dokkaVersions = "1.7.20"
     // https://kotlinlang.org/docs/multiplatform.html
-    val multiplatformVersion = "1.7.10"
+    val multiplatformVersion = "1.8.20-Beta"
     // https://github.com/mpetuska/npm-publish
     val npmPublishVersion = "3.0.3"
     // https://developer.android.com/studio/releases/gradle-plugin#updating-plugin
@@ -40,6 +40,7 @@ buildscript {
         google()
         gradlePluginPortal()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
 
     dependencies {
